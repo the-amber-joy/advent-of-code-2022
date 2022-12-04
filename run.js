de1e2d25aab14f976b5c95603c3041567d00b52d
amberjoy <amber.joy.dev@gmail.com>
2022-12-04 00:20:12 -0600

    day 4 part 2 complete

--- run.js
+++ run.js
@@ -5,6 +5,7 @@ const { d2p2 } = require("./Day 2/part2.js");
 const { d3p1} = require("./Day 3/part1.js");
 const { d3p2 } = require("./Day 3/part2.js");
 const { d4p1 } = require("./Day 4/part1.js");
+const { d4p2 } = require("./Day 4/part2.js");
 
 console.log(d1p1)
 console.log(d1p2)
@@ -13,3 +14,4 @@ console.log(d2p2)
 console.log(d3p1)
 console.log(d3p2)
 console.log(d4p1)
+console.log(d4p2)
