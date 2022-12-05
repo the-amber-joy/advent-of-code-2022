39f910c572d815bc9505fabc4777ec8c9cf0db92
Amber Joy <13399728+the-amber-joy@users.noreply.github.com>
2022-12-05 08:38:59 -0600

    consolidate index.js and run.js

--- run.js
+++ run.js
@@ -1,6 +1,19 @@
-const {
-    results
-} = require("./index.js")
+const fs = require("fs");
+
+const dirs =
+    fs.readdirSync(__dirname, { withFileTypes: true })
+        .filter(dir => dir.isDirectory() && dir.name != ".git" && dir.name != "template")
+        .map(dir => dir.name)
+
+const results = []
+dirs.forEach((dayDir, i) => {
+    results.push(
+        {
+            p1: require(`./${dayDir}/part1.js`),
+            p2: require(`./${dayDir}/part2.js`),
+        }
+    )
+})
 
 results.forEach((day, i) => {
     const part1 = day[`p1`]
